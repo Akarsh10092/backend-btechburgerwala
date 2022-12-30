c6fa3e969e5912492f2be94eb43053646b05c3f7
Akarsh Agarwal <86842321+Akarsh10092@users.noreply.github.com>
2022-12-30 21:48:26 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.get("/googlelogin",passport.authenticate("google",{
 router.get(
     "/login",
      passport.authenticate("google",{
-       successRedirect: process.env.FRONTEND_URL,
+       successRedirect: "https://mbaburgerwala-eight.vercel.app",
       } )
        );
   
